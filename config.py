6df951cc91c53a9f8d3a957288c70f0289e759bf
dariel ramirez perez <130507064+ramirez55@users.noreply.github.com>
2026-08-02 19:08:53 -0400

    Update Telegram bot configuration IDs
    Updated admin user ID and notification chat ID for the Telegram bot.

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ TELEGRAM_BOT_TOKEN = "8715145205:AAEyoe5MNsYDNixfCLDJabHrMrfdDkYMVak"
 
 # Tu ID de usuario de Telegram. Esto es necesario para los comandos de administrador.
 # Puedes obtener tu ID enviando /id a @userinfobot o @getidsbot en Telegram.
-ADMIN_USER_IDS = [TU_ID_DE_USUARIO_ADMIN_AQUI] # Ejemplo: [123456789]
+ADMIN_USER_IDS = [6934424537] # Ejemplo: [123456789]
 
 # El ID del chat o grupo donde el bot enviará las notificaciones de nuevos episodios.
 # Si es un grupo, agrega el bot al grupo, haz que envíe un mensaje,
 # y luego reenvía un mensaje del bot a @getidsbot para obtener el Chat ID.
 # Los IDs de grupo suelen empezar con -100.
-NOTIFICATION_CHAT_ID = TU_ID_DE_CHAT_DE_NOTIFICACIONES_AQUI # Ejemplo: -1001234567890
+NOTIFICATION_CHAT_ID = 6934424537 # Ejemplo: -1001234567890
 
 # --- Intervalo de comprobación ---
 # Frecuencia con la que el bot revisará nuevos episodios (en minutos).
